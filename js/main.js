80cccdf67a21223431b0edceb1cf68fe15e59964
Brandon Melendez <brandonmelendezdev@gmail.com>
2020-02-18 19:51:28 -0500

    finished functionality and aesthetic

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+//rotation animation for arrow in landing page button
 function rotateArrowAnimation() {
   $(".more-btn").on("mouseover", function(e) {
     $("#arrow").addClass("rotate");
@@ -9,18 +10,20 @@ function rotateArrowAnimation() {
   });
 }
 
+// allows user to see more details for each project on hovering over images
 function handleImageHover() {
   $(".project-tile").on("mouseover", function(e) {
     $(".project-overlay", this).css("height", "49%");
     $(".overlay-btn, .overlay-para", this).css("opacity", "0.9");
   });
   $(".project-tile").on("mouseout", function(e) {
-    $(".project-overlay", this).css("height", "0.4%");
+    $(".project-overlay", this).css("height", "0%");
     $(".overlay-btn, .overlay-para", this).css("opacity", "0");
   });
 }
 
-function toggleClass() {
+// reduces or expanded hamburger menu
+function toggleNavExpansion() {
   $("#menu-toggle").click(function() {
     if ($("#navbar").hasClass("reduced")) {
       $("#navbar").removeClass("reduced");
@@ -32,11 +35,33 @@ function toggleClass() {
   });
 }
 
+// adds active class to large navbar to change the color of each link
+function addActiveClass() {
+  const selected = ".navbar-large li a";
+  $(selected).on("click", function(e) {
+    $(selected).removeClass("active");
+    $(this).addClass("active");
+  });
+}
+
+// make navbar sticky when user scrolls
+function makeNavbarSticky() {
+  const navPosition = $("#navbar").offset();
+  $(window).bind("scroll", function() {
+    $(window).scrollTop() > navPosition.top
+      ? $("#navbar").addClass("navbar-sticky")
+      : $("#navbar").removeClass("navbar-sticky");
+  });
+}
+
+
 function loadJavascript() {
-  console.log('javascript loaded...')
+  console.log("javascript loaded...");
   rotateArrowAnimation();
   handleImageHover();
-  toggleClass();
+  toggleNavExpansion();
+  addActiveClass();
+  makeNavbarSticky();
 }
 
 $(loadJavascript);
